49797bb9608e26e52ebdc3677e5921466c0fd4fa
Nicholas Bielecki <nicholas.j.bielecki@gmail.com>
2022-07-31 18:18:08 -0700

    bountyHunterPartOne

--- bountyHunterPartOne/main.js
+++ bountyHunterPartOne/main.js
@@ -1,1 +1,13 @@
-console.log("worked")
+const express = require('express')
+const app = express()
+
+
+app.use(express.json())
+
+app.use("/bounties", require('./routes/bounties.js'))
+
+
+app.listen(5000, ()=>{
+    console.log('The app is running on port 5000')
+})
+
