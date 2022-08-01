2e314bf5832f3bb66592ab09cde0dc4047eef276
Nicholas Bielecki <nicholas.j.bielecki@gmail.com>
2022-07-31 18:20:52 -0700

    073122

---
+++ bountyHunterPartOne/routes/bounties.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const app = express()
+const bountyRouter = express.Router()
+const {v4: uuidv4} = require('uuid')
+
+// bountyRouter.request(express.json())
+app.use(express.json())
+
+
+
+let bounties = [
+    {
+        firstName: "Luke",
+        lastName : "SkyWalker",
+        isAlive : true,
+        bounty : "$500",
+        type : "Jedi",
+        _id : uuidv4()
+    },
+
+    {
+        firstName: "Darth",
+        lastName : "Vader",
+        isAlive : true,
+        bounty : "$1500",
+        type : "Sith",
+        _id : uuidv4()
+    },
+
+    {
+        firstName: "Count",
+        lastName : "Duku",
+        isAlive : true,
+        bounty : "$100",
+        type : "Sith",
+        _id : uuidv4()
+    }
+]
+
+
+
+bountyRouter.get( "", (req, res) =>{
+    res.send(bounties)
+})
+
+bountyRouter.post("", (req,res) =>{
+    const newBounty = req.body
+    console.log(newBounty)
+    newBounty._id = uuidv4()
+    bounties.push(newBounty)
+    res.send(`Sucessfully pushed`)
+})
+
+
+module.exports = bountyRouter
+
